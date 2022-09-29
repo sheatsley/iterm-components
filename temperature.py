ec32342d24399f894762387b3f5ab302ea8224f2
Ryan Sheatsley <ryan@sheatsley.me>
2022-09-28 19:42:10 -0500

    update homebrew dir to apple silicon

--- temperature.py
+++ temperature.py
@@ -1,11 +1,17 @@
 """
 CPU temperature component for iTerm 2
 Author: Ryan Sheatsley
-Sat FEb 29 2020
+Sat Feb 29 2020
 """
 
 import iterm2
+from os import path
 from subprocess import check_output
+
+# check if we're on apple silicon
+root = "/opt/homebrew/bin/"
+if not path.exists(root):
+    root = "/usr/local/bin/"
 
 
 async def main(connection, update_cadence=5):
@@ -23,7 +29,7 @@ async def main(connection, update_cadence=5):
 
     @iterm2.StatusBarRPC
     async def temperature_component(knobs):
-        return check_output("/usr/local/bin/osx-cpu-temp", encoding="utf-8").strip()
+        return check_output(root + "osx-cpu-temp", encoding="utf-8").strip()
 
     await component.async_register(connection, temperature_component)
 
